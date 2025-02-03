8b1de5ee3d42498eeb27fc1e0b274f7a552ce02c
Nick <166817862+ni1ckk@users.noreply.github.com>
2025-02-03 12:04:23 -0300

    Add files via upload

---
+++ EXERCICIO2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MeuSegundoApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+//EXERCÍCIO 2
+        System.Console.WriteLine("Digite o valor do produto: ");
+        double prod= double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Digite a porcentagem do produto: ");
+        double desc= double.Parse(Console.ReadLine());
+
+        double valor= (desc/100) * prod;
+        double valorf= prod - valor; 
+
+        System.Console.WriteLine($"\nO valor do produto é de {valorf} reais");
+
+    }
+}
---
+++ EXERCÍCIO3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MeuSegundoApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //EXERCÍCIO 2
+        int num = 1;
+
+       for (int i = 1; i < 21; i++)
+       {
+        if (num%2==0)
+        {
+           System.Console.WriteLine($"{i}"); 
+        }
+        num++;
+       }
+
+    }
+}
